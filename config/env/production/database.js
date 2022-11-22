455abf74b8d6dbd1f28e6d7a374ddb56e78aeb63
Michael Cillo <mjc.rrnewtech@gmail.com>
2022-11-22 12:19:37 -0500

    some updates

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -1,10 +1,10 @@
 // path: ./config/database.js
-
+// Strapi will use this when it is in production mode. 
 module.exports = ({ env }) => ({
     connection: {
       client: 'postgres',
       connection: {
-        host: env('DATABASE_HOST', '127.0.0.1'),
+        host: env('DATABASE_HOST', '127.0.0.1'), // to be overriden by env var
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
